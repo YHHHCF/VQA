7f40a28107e76ba6ae3cb6dac1c86d37b0e361c0
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-05 18:16:58 +0000

    baseline finished

--- Code/train_val.py
+++ Code/train_val.py
@@ -73,21 +73,21 @@ if __name__ == "__main__":
     criterion = nn.CrossEntropyLoss()
 
     # dataset for train/val VQA
-    # train_set = VqaDataset(var.train_img_path, var.train_img_name_pattern,
-    #                            var.train_ques_path, var.train_ques_embedding_path,
-    #                            var.train_ann_path, var.train_ans_idxs_path)
+    train_set = VqaDataset(var.train_img_path, var.train_img_name_pattern,
+                               var.train_ques_path, var.train_ques_embedding_path,
+                               var.train_ann_path, var.train_ans_idxs_path)
 
     val_set = VqaDataset(var.val_img_path, var.val_img_name_pattern,
                          var.val_ques_path, var.val_ques_embedding_path,
                          var.val_ann_path, var.val_ans_idxs_path)
 
     # data_loader for train/val VQA
-    # train_loader = DataLoader(train_set, batch_size=var.train_batch_size, shuffle=True,
-    #                               num_workers=var.num_workers)
+    train_loader = DataLoader(train_set, batch_size=var.train_batch_size, shuffle=True,
+                                  num_workers=var.num_workers)
     val_loader = DataLoader(val_set, batch_size=var.val_batch_size, shuffle=False, num_workers=var.num_workers)
 
     model.to(var.device)
 
     writer = SummaryWriter()
 
-    train(model, optimizer, criterion, val_loader, val_loader, writer)
+    train(model, optimizer, criterion, train_loader, val_loader, writer)
